a723a5b655f7f3139ed8e74024a83fb6d41e18cc
flowberrys <156359194+flowberrys@users.noreply.github.com>
2025-12-09 14:15:24 -0500

    added logout timer

--- main line page/index.js
+++ main line page/index.js
@@ -4,6 +4,8 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
+let login_attemps = 0
+
 c.fillRect(0,0,canvas.width,canvas.height)
 
 let userName = prompt("please enter user name")
@@ -16,6 +18,7 @@ if (userName = 'darknight25') {
     console.log('no password needed')
     c.fillStyle = 'blue'
     c.fillRect(0,0,canvas.width,canvas.height)
+    login_attemps = 0
     
     
 }
@@ -31,8 +34,28 @@ if(userName = 'darknight' && password !== 'CoCo1435'){
     console.clear
     console.log('login failed try again')
     let retry = prompt("retry password or user name for acsses")
-    console.log("hello" + userName && password + "1")
+    login_attemps + 1
+    console.log("hello" + userName && password + login_attemps)
 
-    document.getElementById('')
+}
+let timer = 120
 
+if (login_attemps => 3) {
+    console.clear
+    c.clearRect
+    c.fillStyle = 'red'
+    c.fillRect(0,0,canvas.width,canvas.height)
+    document.getElementById("h1").innerHTML = "login failed try again in 2 minutes"
+
+const timerInterval = setInterval(() => {
+  counter--; // Decrement the counter by 1
+  console.log(`Remaining: ${counter} seconds`);
+
+  if (counter <= 0) {
+    clearInterval(timerInterval); // Stop the interval when the counter reaches 0 or less
+    console.log("Timer finished! try login again");
+  }
+}, 1000);
+
+ login_attemps = 0
 }

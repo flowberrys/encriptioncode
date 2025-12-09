b8748b45e7ab8c950ac50243b153e8cbe3122da6
flowberrys <156359194+flowberrys@users.noreply.github.com>
2025-12-09 12:58:10 -0500

    g

---
+++ main line page/index.js
@@ -0,0 +1,27 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+
+c.fillRect(0,0,canvas.width,canvas.height)
+
+let userName = prompt("please enter user name")
+console.log("hello" + userName + "!")
+
+if (userName = 'darknight25') {
+    let password = prompt("admin enter password")
+    console.log('admin acsess')
+} else {
+    console.log('no password needed')
+    c.fillStyle = 'cyan'
+    c.fillRect(0,0,canvas.width,canvas.height)
+    
+    
+}
+
+if (password = 'CoCo1435') {
+    c.clearRect(0,0,canvas.width,canvas.height)
+    c.fillStyle = 'blue';
+    c.fillRect(0,0,canvas.width,canvas.height)
+} 
